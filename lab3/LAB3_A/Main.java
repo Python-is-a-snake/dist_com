99b9f0d0febbbe3e8fe2028b3ee7aa6254aa0470
Python-is-a-snake <45516701+Python-is-a-snake@users.noreply.github.com>
2022-12-17 10:36:36 +0200

    Create Main.java

---
+++ lab3/LAB3_A/Main.java
@@ -0,0 +1,21 @@
+package LAB3_A;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static final int BEES_AMOUNT = 5;
+    public static void main(String [] args){
+        Semaphore semaphore = new Semaphore();
+        Pot pot = new Pot(80);
+        List<Thread> bees = new ArrayList<>();
+        for (int i = 1; i < BEES_AMOUNT+1; i++) {
+            Bee bee = new Bee(pot, semaphore);
+            Thread b = new Thread(bee,"Bee #"+i);
+            bees.add(b);
+        }
+        for(Thread b : bees){
+            b.start();
+        }
+    }
+}
